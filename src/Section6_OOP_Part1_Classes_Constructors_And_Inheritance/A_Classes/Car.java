08d18a72ef75b2eef189acc9913c4245abf73f3b
LaurentLch <laurentlonchay@gmail.com>
2021-03-04 19:26:41 +0100

    Section 6 change structure

---
+++ src/Section6_OOP_Part1_Classes_Constructors_And_Inheritance/A_Classes/Car.java
@@ -0,0 +1,15 @@
+package Section6_OOP_Part1_Classes_Constructors_And_Inheritance.A_Classes;
+
+public class Car {
+    // public is an access modifier - means unrestricted access to the class. Other access modifiers are private, protected, etc.
+    // Fields are usually using "private" - Encapsulation. The representation of the object (fields and methods) can be hidden from the outside of the objects definition.
+
+    //FIELDS - INSTANCE VARIABLES - characteristics of the objects
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+
+}
--- src/Section6_OOP_Part1_Classes_Constructors_And_Inheritance/A_Classes_Part1.java
+++
@@ -1,8 +0,0 @@
-package Section6_OOP_Part1_Classes_Constructors_And_Inheritance;
-
-public class A_Classes_Part1 {
-
-    public static void main(String[] args) {
-
-    }
-}
