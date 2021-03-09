0474f14854cc605569886ef5ffc98dc6934fab03
LaurentLch <laurentlonchay@gmail.com>
2021-03-09 15:51:53 +0100

    Composition Exercises Finished

---
+++ src/Section7_OOP_Part2_Composition_Encapsulation_Polymorphism/A_Composition/Exercises/Ceiling.java
@@ -0,0 +1,19 @@
+package Section7_OOP_Part2_Composition_Encapsulation_Polymorphism.A_Composition.Exercises;
+
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
---
+++ src/Section7_OOP_Part2_Composition_Encapsulation_Polymorphism/A_Composition/Exercises/Wall.java
@@ -0,0 +1,15 @@
+package Section7_OOP_Part2_Composition_Encapsulation_Polymorphism.A_Composition.Exercises;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+
+}
